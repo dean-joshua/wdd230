758ad426c891dfaa27a6e4c042859d0f5a894e04
Joshua Dean <dea16006@byui.edu>
2022-05-14 15:10:09 -0600

    updated medium and large views

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,10 +1,20 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
+
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+document.querySelector("#lastmod").textContent = document.lastModified;
+date = new Date();
+document.querySelector("#copyDate").textContent = date.getFullYear();
+
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
 /*** Programming Notes **************************************
   Arrow Functions - es6 syntactically compact alternative to a regular function expression
